3df7380efc75d91f092d2a6605891d4eadbd1e8e
Jeremy Schwartz <jeremy.schwartz@mapmyfitness.com>
2012-04-24 13:48:28 -0500

    New files for Scheem

---
+++ Scheem/parser_test.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var PEG = require('pegjs');
+var assert = require('assert');
+
+parser_text = fs.readFileSync('parser.peg', encoding="ascii");
+
+console.log(parser_text);
+
+
+var parse = PEG.buildParser(parser_text).parse;
+
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
